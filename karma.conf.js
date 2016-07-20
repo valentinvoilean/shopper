3eb152d7c45a6d77d18d24f90118b64e213a8d2c
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-07-20 11:59:29 +0200

    coverage integration

--- karma.conf.js
+++ karma.conf.js
@@ -20,9 +20,6 @@ module.exports = function(config) {
       }
     },
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
@@ -56,12 +53,6 @@ module.exports = function(config) {
       ]
     },
 
-    // list of files to exclude
-    exclude: [],
-
-    // list of files / patterns to load in the browser
-    files: [],
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [
@@ -80,19 +71,6 @@ module.exports = function(config) {
         'src/js/**/*.js'
       ]
     },
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    plugins: [
-      'karma-babel-preprocessor',
-      'karma-coverage',
-      'karma-jasmine',
-      'karma-jspm',
-      'karma-phantomjs-launcher',
-      'karma-spec-reporter'
-    ],
 
     // web server port
     port: 9876,
@@ -114,6 +92,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: true
   });
 };
