fd8f5317b52c534b1153ee2e3ce9753b4393b567
Valentin Voilean <valentin.voilean@netcentric.biz>
2016-08-03 00:20:15 +0200

    started currency component

--- src/js/components/currency/index.js
+++ src/js/components/currency/index.js
@@ -2,12 +2,54 @@ import $ from 'jquery';
 
 window.ss = window.ss || {};
 
-ss.Currencies = class {
+let mediaQueriesConfig = {
+  xs() { console.warn('xs'); },
+  sm() { console.warn('sm'); },
+  md() { console.warn('md'); },
+  lg() { console.warn('lg'); },
+  xsMin() { console.warn('xsMin'); },
+  smMin() { console.warn('smMin'); },
+  mdMin() { console.warn('mdMin'); },
+  lgMin() { console.warn('lgMin'); },
+  xsMax() { console.warn('xsMax'); },
+  smMax() { console.warn('smMax'); },
+  mdMax() { console.warn('mdMax'); }
+};
+
+
+class setMediaQueryCallbacks {
+  constructor(mediaQueriesConfig) {
+    this.MQ_CONFIG = mediaQueriesConfig;
+    this.addCallbacksToMQEvents();
+  }
+
+  destroy() {
+    this.removeCallbacksFromMQEvents();
+  }
+
+  addCallbacksToMQEvents() {
+    $.each(this.MQ_CONFIG, function(key, value) {
+      $(ss).on(key, value);
+    });
+  }
+
+  removeCallbacksFromMQEvents() {
+    $.each(this.MQ_CONFIG, function(key, value) {
+      $(ss).on(key, value);
+    });
+  }
+}
+
+ss.Currencies = class extends setMediaQueryCallbacks {
   constructor() {
     $.getScript('/services/javascripts/currencies.js', function () {
       console.warn(Currency);
     });
+    super(mediaQueriesConfig);
+  }
 
+  destroy() {
+    super.destroy();
   }
 
 };
