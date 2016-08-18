ae5a14435f0ab3dcd61cc76cc297a6786153a77d
VALENTIN-MARIAN VOILEAN <vali.voilean@gmail.com>
2016-08-18 10:17:56 +0200

    merged desktop&tablet versions for myaccount top header

--- src/js/top-header-my-account/top-header-my-account.component.js
+++ src/js/top-header-my-account/top-header-my-account.component.js
@@ -1,20 +1,120 @@
-import Tablet from './top-header-my-account.tablet.component';
-import Desktop from './top-header-my-account.desktop.component';
+import {SHARED_CLASSES} from 'js/shared/shared';
+import {CLASSES} from './top-header-my-account.config';
 
 export default class TopHeaderMyAccountComponent {
   constructor($el) {
     this.$el = $el;
+    this.$link = $el.find(`.${CLASSES.link}`);
+    this.$leftSide = $el.find(`.${CLASSES.leftSide}`);
+    this.$rightSide = $el.find(`.${CLASSES.rightSide}`);
+    this.$welcomeMessage = this.$rightSide.find(`.${CLASSES.link}`);
 
-    if (Modernizr.touchevents) {
-      this.instance = new Tablet(this.$el);
-    } else {
-      this.instance = new Desktop(this.$el);
-    }
+    // We add the animation class after initialization because
+    // if we want to destroy it later to not wait until the animation finishes
+    this.$el.addClass(SHARED_CLASSES.animate);
+    this.$leftSide.addClass(SHARED_CLASSES.animate);
+    this.$welcomeMessage.addClass(SHARED_CLASSES.animate);
+
+    this._calculateWidths();
+    this._addEventHandlers();
   }
 
   destroy() {
-    this.instance.destroy();
+    this._removeEventHandlers();
+    this.$el.add(this.$leftSide).add(this.$welcomeMessage).removeClass(SHARED_CLASSES.animate);
+    this.$el.removeClass(SHARED_CLASSES.active);
+    this.$leftSide.add(this.$welcomeMessage).width('');
     this.$el = null;
-    this.instance = null;
+    this.$leftSide = null;
+    this.$rightSide = null;
+    this.$welcomeMessage = null;
+  }
+
+  _calculateWidths() {
+    this.$leftSide.attr('data-width', this.$leftSide.outerWidth());
+    this.$welcomeMessage.attr('data-width', this.$welcomeMessage.outerWidth());
+    this.$leftSide.width(0).addClass(SHARED_CLASSES.visible);
+    this.$welcomeMessage.width(this.$welcomeMessage.data('width'));
+  }
+
+  _addEventHandlers() {
+    if (Modernizr.touchevents) {
+      this.$el.on('click', $.proxy(this._activateItem, this));
+      $(document).on('click', $.proxy(this._deactivateItem, this));
+      this.$link.on('click', this._activateLink);
+    } else {
+      this.$el.on('mouseover', $.proxy(this._activateItem, this));
+      this.$el.on('mouseout', $.proxy(this._deactivateItem, this));
+      this.$link.on('mouseover', this._activateLink);
+    }
+  }
+
+  _removeEventHandlers() {
+    if (Modernizr.touchevents) {
+      this.$el.off('click', $.proxy(this._activateItem, this));
+      $(document).off('click', $.proxy(this._deactivateItem, this));
+      this.$link.off('click', this._activateLink);
+    } else {
+      this.$el.off('mouseover', $.proxy(this._activateItem, this));
+      this.$el.off('mouseout', $.proxy(this._deactivateItem, this));
+      this.$link.off('mouseover', this._activateLink);
+    }
+  }
+
+  _activateLink(e) {
+    $(this).addClass(SHARED_CLASSES.active);
+    $(this).siblings().removeClass(SHARED_CLASSES.active);
+
+    if (Modernizr.touchevents) {
+      e.stopPropagation();
+    }
+  }
+
+  _activateItem(e) {
+    if (Modernizr.touchevents) {
+      this._preventClickFirstTime(e);
+    } else {
+      this._slideInLeftSide();
+    }
+  }
+
+  _deactivateItem(e) {
+    if (Modernizr.touchevents) {
+      if (!this.$el.is(e.target) // if the target of the click isn't the container...
+        && this.$el.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+        this._slideOutLeftSide();
+      }
+    } else {
+      this._slideOutLeftSide();
+    }
+  }
+
+  _preventClickFirstTime(e) {
+    if (this.$el.hasClass(SHARED_CLASSES.active)) {
+      return true;
+    }
+    else {
+      e.preventDefault();
+      this.$el.addClass(SHARED_CLASSES.active);
+      this._slideInLeftSide();
+    }
+  }
+
+  _slideInLeftSide() {
+    this.$leftSide
+      .one('transitionend', () => {
+        this.$welcomeMessage.width(0);
+      })
+      .width(this.$leftSide.data('width'));
+  }
+
+  _slideOutLeftSide() {
+    this.$leftSide
+      .one('transitionend', () => {
+        this.$welcomeMessage.width(this.$welcomeMessage.data('width'));
+        this.$el.removeClass(SHARED_CLASSES.active);
+      })
+      .width(0);
   }
 };
