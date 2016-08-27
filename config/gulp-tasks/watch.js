54c28794f02a3c322996190d00b765241c4f2e64
Valentin-Marian Voilean <vali.voilean@gmail.com>
2016-08-27 12:18:53 +0200

    updated gulp tasks

--- config/gulp-tasks/watch.js
+++ config/gulp-tasks/watch.js
@@ -3,20 +3,25 @@
 let apiConfig = require(`${__config}/shopifyApiConfiguration`);
 
 module.exports = function (gulp, plugins) {
-  plugins.watch(`${__theme}/**`)
+  plugins.watch(`${__theme}/**`, (file) => plugins.util.log(`Modified THEME file: ${file.path}`))
+    .pipe(plugins.plumber())
     .pipe(plugins.shopifyUpload(apiConfig.key, apiConfig.password, apiConfig.siteName, apiConfig.themeID,
       {basePath: __theme})
-    );
+    )
+    .pipe(plugins.plumber.stop());
 
-  plugins.watch(`${__src.sass}/**/*.*`, function () {
+  plugins.watch(`${__src.sass}/**/*.*`, (file) => {
+    plugins.util.log(`Modified SASS file: ${file.path}`);
     plugins.runSequence('concatSass');
   });
 
-  plugins.watch(`${__src.js}/**/*.*`, function () {
+  plugins.watch(`${__src.js}/**/*.*`, (file) => {
+    plugins.util.log(`Modified JS file: ${file.path}`);
     plugins.runSequence(['lint', 'createBundle']);
   });
 
-  plugins.watch(`${__tests}/**/*.*`, function () {
+  plugins.watch(`${__tests}/**/*.*`, (file) => {
+    plugins.util.log(`Modified Test file: ${file.path}`);
     plugins.runSequence('lint');
   });
 };
