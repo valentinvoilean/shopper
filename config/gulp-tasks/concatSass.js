3f261ce60768606c82051c0453cbd4475e2e7908
VALENTIN-MARIAN VOILEAN <vali.voilean@gmail.com>
2016-09-29 15:14:49 +0200

    updated npm packages

--- config/gulp-tasks/concatSass.js
+++ config/gulp-tasks/concatSass.js
@@ -5,6 +5,7 @@ module.exports = function (gulp, plugins) {
     gulp.src(`${__src.sass}/*.scss`)
       .pipe(plugins.shopifySass())
       .pipe(plugins.rename(function (path) {
+        path.basename = path.basename.split('.cat')[0];
         path.extname = '.scss.liquid';
       }))
       .pipe(gulp.dest(__assets));
