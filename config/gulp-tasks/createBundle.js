1eadc0908f65bb0f69def20b27ef129b8ad8bec3
Valentin-Marian Voilean <vali.voilean@gmail.com>
2016-10-14 12:19:47 +0200

    updated gulp

--- config/gulp-tasks/createBundle.js
+++ config/gulp-tasks/createBundle.js
@@ -2,10 +2,10 @@
 
 module.exports = function (gulp, plugins) {
     return () =>
-        gulp.src(`${__src.js}/main.jsx`)
+        gulp.src(`${__src.js}/main.js`)
             .pipe(plugins.webpack(require(`${__base}/webpack.config.js`)))
             .pipe(plugins.rename(function (path) {
-                path.extname = path.extname == '.js' ? '.js.liquid': path.extname;
+                path.extname = path.extname === '.js' ? '.js.liquid': path.extname;
             }))
             .pipe(gulp.dest(__assets));
 };
