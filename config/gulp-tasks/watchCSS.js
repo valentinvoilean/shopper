9e547b31f77b01620f11e21fb13e31b1cae19091
Valentin-Marian Voilean <vali.voilean@gmail.com>
2016-10-15 11:03:17 +0200

    updated package.json

--- config/gulp-tasks/watchCSS.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (gulp, plugins) {
-    return () =>
-        plugins.watch(`${__src.sass}/**/*.*`, (file) => {
-            plugins.util.log(`Modified SASS file: ${file.path}`);
-            plugins.runSequence('concatSass');
-        });
-};
--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,10 @@ const gulp = require('gulp'),
 
 // Gulp Tasks
 
-// SASS related Tasks
-gulp.task('normalizeCSS', require(`${__gulpTasks}/normalizeCSS/normalizeCSS`)(gulp, plugins));
-gulp.task('concatSass', require(`${__gulpTasks}/concatSass`)(gulp, plugins));
-gulp.task('compile:sass', ['normalizeCSS', 'concatSass']);
-
 // Unit tests related tasks
 gulp.task('test', require(`${__gulpTasks}/test`)());
 gulp.task('test:debug', require(`${__gulpTasks}/test`)(true));
 
-gulp.task('watch:sass', require(`${__gulpTasks}/watchCSS`)(gulp, plugins));
 gulp.task('lint', require(`${__gulpTasks}/lint`)(gulp, plugins));
 
-gulp.task('default', ['watch:sass', 'compile:sass']);
+gulp.task('default', ['lint']);
